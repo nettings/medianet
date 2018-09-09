88ce4c33907b30a9ee68bbe6a1beda1845d43567
Jörn Nettingsmeier <nettings@luchtbeweging.nl>
2018-09-09 21:43:22 +0000

    enable zam-comp in default configuration

--- mod-host.cmd
+++ mod-host.cmd
@@ -1,8 +1,11 @@
 # medianet mod-host configuration
 add http://gareus.org/oss/lv2/fil4#stereo 1
-add http://stackingdwarves.net/lv2/sm 2
-connect effect_1:outL effect_2:inL
-connect effect_1:outR effect_2:inR
+add urn:zamaudio:ZamCompX2 2
+add http://stackingdwarves.net/lv2/sm 3
+connect effect_1:outL effect_2:lv2_audio_in_1
+connect effect_1:outR effect_2:lv2_audio_in_2
+connect effect_2:lv2_audio_out_1 effect_2:inL
+connect effect_2:lv2_audio_out_2 effect_2:inR
 connect effect_2:outL system:playback_1
 connect effect_2:outR system:playback_2
 connect zita-n2j:out_1 effect_1:inL
