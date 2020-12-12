4c300771861e72036455a8fb6af8d198f5fbad76
Jörn Nettingsmeier <nettings@luchtbeweging.nl>
2020-12-12 21:01:32 +0000

    restructure web interfaces, add rudimentary landing page

---
+++ overlay/var/www/html/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8"/>
+    <title>[mn] medianet Web</title>
+  </head>
+  <body>
+    <h1>[mm] medianet Web</h1>
+    <ul>
+      <li><a href="medianet/DSP/">Generic plugin web interface</a></li>
+      <li><a href="medianet/Tunnel/">Maintenance tunnel control interface</a></li>
+      <li><a href="http://<?php echo $_SERVER['SERVER_NAME']?>:8000/status.xsl">Audio monitor stream server</a></li>
+    </ul>
+  </body>
+</html>
+
