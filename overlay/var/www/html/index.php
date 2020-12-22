59c1baa56ba3899b314b09dcc5e8421fb04cdac8
Jörn Nettingsmeier <nettings@luchtbeweging.nl>
2020-12-22 22:17:58 +0000

    automatically create web landing page with links to all web services

--- overlay/var/www/html/index.php
+++ overlay/var/www/html/index.php
@@ -5,11 +5,39 @@
     <title>[mn] medianet Web</title>
   </head>
   <body>
-    <h1>[mm] medianet Web</h1>
+    <h1>[mn] medianet Web</h1>
     <ul>
-      <li><a href="medianet/DSP/">Generic plugin web interface</a></li>
-      <li><a href="medianet/Tunnel/">Maintenance tunnel control interface</a></li>
-      <li><a href="http://<?php echo $_SERVER['SERVER_NAME']?>:8000/status.xsl">Audio monitor stream server</a></li>
+<?php
+  error_reporting(E_ALL);
+  define('FEATURES_DIR', __DIR__ . '/medianet_features.d');
+  define('HOSTNAME_TAG', '%HOSTNAME%');
+  define('EOL_CHARS', "\r\n");
+  if ($feature_files = scandir(FEATURES_DIR)) {;
+    foreach($feature_files as $file) {
+      if ($file == '.' or $file == '..') continue;
+      if ($fp = fopen(FEATURES_DIR . "/$file", 'r')) {
+        $link = rtrim(
+          str_replace(
+            HOSTNAME_TAG,
+            $_SERVER['SERVER_NAME'],
+            fgets($fp)
+          ),
+          EOL_CHARS
+        );
+        $text = rtrim(
+          fgets($fp),
+          EOL_CHARS
+        );
+        fclose($fp);
+        print("<li><a href=\"$link\">$text</a></li>\n");
+      } else {
+        print("<li><strong>ERROR:</strong> could not open file " . $file . "</li>\n");
+      }
+    }
+  } else {
+    print("<li><strong>ERROR:</strong> could not read directory " . FEATURES_DIR . "</li>\n");
+  }
+?>
     </ul>
   </body>
 </html>
